87f3cee584b7c6ca474f688d1587cea8a524e06d
PortSeventySix <30667844+PortSeventySix@users.noreply.github.com>
2022-12-14 09:32:59 +0000

    Reproduction of issue

---
+++ Elastic APM Config Issue/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Elastic_APM_Config_Issue
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ Elastic APM Config Issue/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Elastic_APM_Config_Issue.MvcApplication" Language="C#" %>
